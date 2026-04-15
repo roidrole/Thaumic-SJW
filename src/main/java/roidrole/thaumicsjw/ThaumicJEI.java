33b937abbb3984361cd3d3cae1ecd95a6c00b41a
roidrole <83431927+roidrole@users.noreply.github.com>
2026-04-14 23:36:00 -0400

    Minimal README, fix MMCE incompat

---
+++ src/main/java/roidrole/thaumicsjw/ThaumicJEI.java
@@ -0,0 +1,10 @@
+package roidrole.thaumicsjw;
+
+import net.minecraftforge.fml.common.Mod;
+
+//This file is only included so modular magic doesn't register an AspectList JEI handler that crash due to multiple IIngredientType for the same class
+@Mod(
+	modid = "thaumicjei"
+)
+public class ThaumicJEI {
+}
